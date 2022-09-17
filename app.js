b5a7d9c914082fca0de1a09a54a9b87ac7ed9734
DANIEL <107651766+Anuebunwa-Daniel@users.noreply.github.com>
2022-09-17 17:43:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.use('/assets', express.static('assets'))
-app.use(express.urlencoded ({extended:true}))
+app.use(express.urlencoded ({extended:false}))
 
 app.get('/', (req, res)=>{
     res.render('clock')
